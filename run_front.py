eaf523a0d8b93826f80db1fb706b3388f0c2e8b6
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2019-08-24 23:01:47 -0400

    trying opencv

--- run_front.py
+++ run_front.py
@@ -51,6 +51,7 @@ print("Hola!")
 def pong():
     return 'pong'
 
+
 @app.route('/img')
 def img():
     with open('/home/ubuntu/BoatsBeGone/boat_vid.mp4', "wb") as file:
@@ -58,11 +59,11 @@ def img():
         response = requests.get('http://www.quebec511.info/Carte/Fenetres/camera.ashx?id=3379&format=mp4')
         # write to file
         file.write(response.content)
-        vidcap = cv2.VideoCapture('vid.mp4')
+        vidcap = cv2.VideoCapture('/home/ubuntu/BoatsBeGone/boat_vid.mp4')
         success, image = vidcap.read()
         if success:
             cv2.imwrite("/home/ubuntu/BoatsBeGone/boat_frame.jpg", image)
-            return send_file('boat_frame.jpg',mimetype = 'image/jpeg')
+            return send_file('boat_frame.jpg', mimetype='image/jpeg')
         else:
             return "error"
 
